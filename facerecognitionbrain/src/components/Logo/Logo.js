eaecbfcb9fa1165912f527e2b441b64a9b9a8e81
prayat-pu <bosssds65@gmail.com>
2020-11-29 12:00:05 +0700

    face recognition web frontend

---
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = ()=>{
+    return (
+        <div class='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner"> 
+                    <img style={{paddingTop:'5px'}} alt='logo' src={brain}/>
+                 </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;

ab7ce6741301275f5abaa6af3ad52303bbc5305b
prayat-pu <bosssds65@gmail.com>
2020-12-02 02:37:33 +0700

    complete frontend project

--- facerecognitionbrain/src/components/Logo/Logo.js
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -4,9 +4,9 @@ import brain from './brain.png';
 
 const Logo = ()=>{
     return (
-        <div class='ma4 mt0'>
+        <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"> 
+                <div className="Tilt-inner">
                     <img style={{paddingTop:'5px'}} alt='logo' src={brain}/>
                  </div>
             </Tilt>
